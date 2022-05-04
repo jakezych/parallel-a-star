d449844c38f451fade04f38f5a46c79455663d75
Jacob Justin Zych <zych.jake@gmail.com>
2022-05-04 01:45:06 -0400

    finished pathGrapher

--- code/PathGrapher.java
+++ code/PathGrapher.java
@@ -8,151 +8,175 @@ import java.awt.image.BufferedImage;
 
 public class PathGrapher extends JFrame {
 
-    private String fileName = null;
-    private int width = 740;
-    private int height = 740;
-    private float scaleX = 0.0f;
-    private float scaleY = 0.0f;
-    private int maxX = 0;
-    private int maxY = 0;
+  private String inputFileName = null;
+  private String outputFileName = null;
+  private int width = 800;
+  private int height = 800;
+  private int gridRows;
+  private int gridCols;
 
-    private ArrayList<Ellipse2D.Float> endPoints = null;
-    private ArrayList<Line2D.Float> path = null;
+  private int[][] grid; 
 
-    private static final int dotRadius = 2;
-    private static final int pathSize = 2;
-    private static final int boundaries = 50;
-    private static final Color bgColor = Color.LIGHT_GRAY;
-    private static final Color dotColor = Color.black;
-    private static final Color pathColor = Color.blue;
+  private static final int margin = 25;
+  private static final int outlineWidth = 1;
 
-    private BufferedImage bufImg;
+  private static final Color obstacleColor = Color.black;
+  private static final Color nodeColor = Color.LIGHT_GRAY;
+  private static final Color outlineColor = Color.white;
+  private static final Color pathColor = Color.blue;
 
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
+  private BufferedImage bufImg;
 
-    public void setDimensions(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
+  public void setInputFileName(String fileName) {
+    this.inputFileName = fileName;
+  }
 
-    public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D)g;
-        g2d.drawImage(bufImg, null, 0, 0);
-    }
+  public void setOutputFileName(String fileName) {
+    this.outputFileName = fileName;
+  }
 
-    public void paintPicture(Graphics g) {
-        Graphics2D g2d = (Graphics2D)g;
-        Iterator<Line2D.Float> i;
-        System.out.println("painting...");
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                             RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.setColor(bgColor);
-        g2d.fillRect(0, 0, width, height);
-        g2d.setColor(pathColor);
+  public void setCanvasDimensions(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
 
-        Line2D.Float segment;
-        i = path.iterator();
-        while (i.hasNext()) {
-          segment = i.next(); 
-          g2d.draw(segment);
-        }
+  public void setGridDimensions(int rows, int cols) {
+    this.gridRows = rows;
+    this.gridCols = cols;
+  }
 
-        g2d.setColor(dotColor);
-        g2d.fill(endPoints.get(0));
-        g2d.fill(endPoints.get(1));
-    }
+  public void initializeGrid(int dim) {
+    this.grid = new int[dim][dim];
+  }
 
-    public void go() {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(fileName));
-            String line;
-            String[] points;
-            // first line unnecessary for graphing 
-            br.readLine();
+  public void paint(Graphics g) {
+    Graphics2D g2d = (Graphics2D)g;
+    g2d.drawImage(bufImg, null, 0, 0);
+  }
 
-            // TODO: check these lines again
-            scaleX = (width - 2.0f * boundaries) / this.width;
-            scaleY = (height - 2.0f * boundaries) / this.height;
+  public void draw() {
+    bufImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+    this.setCanvasDimensions(width, height);
+    this.setSize(width, height);
+    this.setResizable(false);
 
-            // line with path 
-            line = br.readLine().trim();
-            points = line.split(" ");
+    paintGraph(bufImg.createGraphics());
 
-            ArrayList<Line2D.Float> path = new ArrayList<Line2D.Float>();
-            ArrayList<Ellipse2D.Float> endPoints = new ArrayList<Ellipse2D.Float>();
-            
-            String source = points[0];             
+    setVisible(true);
 
-            int r0, c0, r1, c1;
-            Float coordX0, coordY0, coordX1, coordY1;
-            r1 = Character.getNumericValue(source.charAt(1));
-            c1 = Character.getNumericValue(source.charAt(3));
+    addWindowListener(new WindowAdapter() {
+        public void windowClosing(WindowEvent e) { System.exit(0); }
+    });
+  }
 
-            coordX1 = boundaries + scaleX * r1 - dotRadius;
-            coordY1 = boundaries + scaleY * c1 - dotRadius;
+  public int[] getCoordinates(int i, int j) {
+    int[] coords = new int[4];
+    int scaledWidth = width - 2*margin;
+    int scaledHeight = height - 2*margin;
+    coords[0] = margin + (scaledWidth * j) / gridCols;
+    coords[1] = margin + (scaledHeight * i) / gridRows;
+    coords[2] = margin + (scaledWidth * (j + 1)) / gridCols;
+    coords[3] = margin + (scaledHeight * (i + 1)) / gridRows;
+    return coords;
+  }
 
-            // add starting dot 
-            Ellipse2D.Float dotStart = new Ellipse2D.Float(coordX1, coordY1, 2 * dotRadius, 2 * dotRadius);
-            endPoints.add(dotStart);
-
-            coordX1 += dotRadius;
-            coordY1 += dotRadius;
-
-            for(int i = 1; i < points.length; i++) {
-              r0 = r1;
-              c0 = c1;
-              coordX0 = coordX1;
-              coordY0 = coordY1;
-              String point = points[i];
-
-              r1 = Character.getNumericValue(point.charAt(1));
-              c1 = Character.getNumericValue(point.charAt(3));
-              coordX1 = boundaries + scaleX * r1;
-              coordY1 = boundaries + scaleX * c1;
-              Line2D.Float segment = new Line2D.Float(coordX0, coordY0, coordX1, coordY1);
-              path.add(segment);
-            }
-
-            coordX1 -= dotRadius;
-            coordY1 -= dotRadius;
-
-            Ellipse2D.Float dotEnd = new Ellipse2D.Float(coordX1, coordY1, 2 * dotRadius, 2 * dotRadius);
-            endPoints.add(dotEnd);
-      
-            this.endPoints = endPoints;
-            this.path = path;
-            this.setDimensions(width, height);
-            this.setSize(width, height);
-            this.setResizable(false);
-            this.setBackground(bgColor);
-
-            // compute image
-            bufImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-            paintPicture(bufImg.createGraphics());
-
-            setVisible(true);
-
-            addWindowListener(new WindowAdapter() {
-                public void windowClosing(WindowEvent e) { System.exit(0); }
-            });
-        } catch (Exception exc) {
-            exc.printStackTrace();
-            System.err.println(exc.getMessage());
-        }
-    }
-
-    // TODO: account to obstacles!!! 
-    public static void main(String args[]) {
-        PathGrapher g = new PathGrapher();
-        if (args.length < 3) {
-          System.out.println("usage: java PathGrapher [output_filename] [rows] [cols] \n Visualizes the path output of an execution of A*\n");
+  public void paintGraph(Graphics g) {
+    Graphics2D g2d = (Graphics2D)g;
+    System.out.println("painting...");
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                          RenderingHints.VALUE_ANTIALIAS_ON);
+    g2d.setStroke(new BasicStroke(outlineWidth));
+    g2d.setColor(Color.DARK_GRAY);
+    g2d.fillRect(0, 0, width, height);
+    for (int i = 0; i < gridRows; i++) {
+      for (int j = 0; j < gridCols; j++) {
+        int node = grid[i][j];
+        int[] coords = getCoordinates(i, j);
+        // choose color based on node type 
+        if (node == 0) {
+          g2d.setColor(obstacleColor);
+        } else if (node == 1) {
+          g2d.setColor(nodeColor);
         } else {
-          g.setFileName(args[0]);
-          g.setDimensions(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+          g2d.setColor(pathColor);
         }
-
-        g.go();
+        // fill rectangle
+        g2d.fillRect(coords[0], coords[1], coords[2] - coords[0], coords[3] - coords[1]);
+        g2d.setColor(outlineColor);
+        // draw outline
+        g2d.drawRect(coords[0], coords[1], coords[2] - coords[0], coords[3] - coords[1]);
+      }
     }
+  }
+
+  public void readGraph() {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(inputFileName));
+      String line = br.readLine().trim(); 
+      // read dimensions
+      int dim = Integer.parseInt(line);
+
+      setGridDimensions(dim, dim);
+      initializeGrid(dim);
+      for (int i = 0; i < dim; i++) {
+        line = br.readLine().trim();
+        String[] elems = line.split(" ");
+        for (int j = 0; j < dim; j++) {
+          int node = Character.getNumericValue(elems[j].charAt(0));
+          System.out.println("i: " + i + " j: " + j + " node: " + node);
+          this.grid[i][j] = node;
+        }
+      }
+    } catch (Exception exc) {
+      exc.printStackTrace();
+      System.err.println(exc.getMessage());
+    }
+  }
+
+  public void readPath() {
+    try {
+      BufferedReader br = new BufferedReader(new FileReader(outputFileName));
+      String line;
+      String[] points;
+      // first line unnecessary for graphing 
+      br.readLine();
+
+      // line with path 
+      line = br.readLine().trim();
+      points = line.split(" ");
+
+      for(int i = 0; i < points.length; i++) {
+        // need to account for multiple digit rows and cols
+        int commaIndex = points[i].indexOf(",");
+        String rowString = points[i].substring(1, commaIndex);
+        String colString = points[i].substring(commaIndex + 1, points[i].length() - 1);
+        System.out.println("row string: " + rowString + " col string: " + colString);
+        int row = Integer.parseInt(rowString);
+        int col = Integer.parseInt(colString);
+        // path is represented by 2s in the grid 
+        this.grid[row][col] = 2;
+      }
+
+  } catch (Exception exc) {
+      exc.printStackTrace();
+      System.err.println(exc.getMessage());
+    }
+  }
+
+
+  public static void main(String args[]) {
+    PathGrapher g = new PathGrapher();
+    if (args.length < 2) {
+      System.out.println("usage: java PathGrapher [input_filename] [output_filename] \n Visualizes the path output of an execution of A*\n");
+    } else {
+      g.setInputFileName(args[0]);
+      g.setOutputFileName(args[1]);
+      if (args.length == 4) {
+        g.setCanvasDimensions(Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+      }
+    }
+    g.readGraph();
+    g.readPath();
+    g.draw();
+  }
 }
